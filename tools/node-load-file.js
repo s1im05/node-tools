7c9dbec16d361bd03b7ba0964768d0f66bc5b5b2
Vadim Suleimanov <visulej2@mts.ru>
2017-10-31 15:08:52 +0300

    Load Files tool added, with sample

---
+++ tools/node-load-file.js
@@ -0,0 +1,43 @@
+const http = require('http'),
+	path = require('path'),
+	request = require('request'),
+	fs = require('fs'),
+	url = require('url'),
+	parseParams = (input) => {
+		const query = url.parse(input).query,
+			params = {};
+		if (query) {
+			query.split('&').forEach(param => {
+				const keyVal = param.split('=');
+				params[keyVal[0]] = keyVal[1];
+			});
+		}
+		return params;
+	},
+	server = http.createServer((req, res) => {
+		const params = parseParams(req.url);
+		if (+params.id) {
+			console.log('Copy ID:', params.id);
+			console.log('Src:', params.src);
+			console.log('Destination:', params.to);
+			console.log('------------------------------------');
+			
+			const dirToFull = params.to + '/' + params.id;
+			try {
+				fs.accessSync(dirToFull);
+			} catch (err) {
+				fs.mkdirSync(dirToFull);
+			}
+			const file = path.basename(params.src);
+			request(params.src).pipe(fs.createWriteStream(dirToFull + '/' + file));			
+		}
+		res.end();
+	}),
+    port = 8383;
+	
+server.on('clientError', (err, socket) => {
+	socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+server.listen(port);
+console.log('We are live on http://localhost:' + port);
+console.log('====================================');
